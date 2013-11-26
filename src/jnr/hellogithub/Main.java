895c89195cb6f6cab38503286d57e31f178f7cce
JnrNR <jnr_bluehat@hotmail.com>
2013-11-26 13:13:16 -0600

    Primer commit

---
+++ src/jnr/hellogithub/Main.java
@@ -0,0 +1,17 @@
+package jnr.hellogithub;
+/**
+ *
+ * @author Jnr
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hola GitHub");
+        
+        System.out.println("La suma es: " + suma(5,6));
+    }
+    
+    public static int suma(int a, int b){
+        return a+b;
+    }
+}
