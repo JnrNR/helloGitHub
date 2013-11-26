fab5c1072f71fb9005342cc13bd8eacbbb0fe1f8
JnrNR <jnr_bluehat@hotmail.com>
2013-11-26 16:54:14 -0600

    Agregué multiplicacion y modifique main para hacer uso de ella

--- src/jnr/hellogithub/Main.java
+++ src/jnr/hellogithub/Main.java
@@ -13,11 +13,14 @@ public class Main {
         System.out.println("La suma es: " + suma(a,b));
         
         Resta objetoResta = new Resta();
+        Multiplicacion mul = new Multiplicacion();
         
         objetoResta.cantidad1 = 20;
         objetoResta.cantidad2 = 5;
         
         System.out.println("La resta es: " + objetoResta.resta());
+        System.out.println("5*5="+mul.multiplica());
+        
     }
     
     public static int suma(int a, int b){
---
+++ src/jnr/hellogithub/Multiplicacion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jnr.hellogithub;
+
+/**
+ *
+ * @author Jnr
+ */
+public class Multiplicacion {
+    
+    public int multiplica(){
+    
+        return 5*5;
+    }
+    
+}
